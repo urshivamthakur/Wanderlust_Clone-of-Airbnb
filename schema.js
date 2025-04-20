420adf583dcb26f0f383a6ef50bbd0570cd0cbd7
urshivamthakur <shivamthakur0620@gamil.com>
2025-04-20 11:01:48 +0530

    Review Model (Phase-2 (part a))

--- schema.js
+++ schema.js
@@ -12,3 +12,9 @@ const listingSchema = Joi.object({
 });
 
 module.exports = { listingSchema };
+module.exports.reviewSchema = Joi.object({
+  review: Joi.object({
+    rating: Joi.number().required().min(1).max(5),
+    comment: Joi.string().required()
+  }).required()
+})
